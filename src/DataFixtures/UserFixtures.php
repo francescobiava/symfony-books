9899338652a58d60c21a0f36ec9adab69ef5b966
Francesco Biava <55140765+francns@users.noreply.github.com>
2020-08-24 15:33:48 +0200

    added login

---
+++ src/DataFixtures/UserFixtures.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\DataFixtures;
+
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use Faker\Factory;
+use Faker\Generator;
+use App\Entity\User;
+
+class UserFixtures extends Fixture
+{
+    private $passwordEncoder;
+
+    public function __construct(UserPasswordEncoderInterface $passwordEncoder)
+    {
+        $this->passwordEncoder = $passwordEncoder;
+    }
+
+    public function load(ObjectManager $manager)
+    {
+        for ($i = 0; $i < 25; $i++) {
+            $user = new User();
+            $faker = Factory::create();
+            $email = $faker->email;
+            $password = 'password';
+
+            $user->setEmail($email)->setPassword($this->passwordEncoder->encodePassword($user, $password));
+
+            $manager->persist($user);
+        }
+        
+        $manager->flush();
+    }
+}
